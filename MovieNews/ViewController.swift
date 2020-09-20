76d2653bf624a8b31b115f408a58daeeb1ceaba1
olzhas <toleugazin0@gmail.com>
2020-09-20 15:38:03 +0600

    Initial Commit

---
+++ MovieNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieNews
+//
+//  Created by Olzhas Toleugazin on 9/20/20.
+//  Copyright © 2020 Olzhas Toleugazin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
